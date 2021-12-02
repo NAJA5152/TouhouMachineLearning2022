8ad63ac59e16c44907c93019a43fac675e0d75c0
red-gezi <506065448@qq.com>
2021-12-03 00:50:37 +0800

    完成故事模式转换为json的规则

--- OtherSolution/xls检测更新/Program.cs
+++ OtherSolution/xls检测更新/Program.cs
@@ -19,7 +19,7 @@ namespace xls检测更新
         static void Main(string[] args)
         {
             Workbook workbook = new Workbook();
-            direPath = Directory.GetCurrentDirectory().Replace(@"OtherSolution\xls检测更新\bin\Debug\net461", @"Assets\Resources\CardData\");
+            direPath = Directory.GetCurrentDirectory().Replace(@"OtherSolution\xls检测更新\bin\Debug\net461", @"Assets\Resources\GameData\");
             filePath = direPath + "GameData.xlsx";
             Task.Run(async () =>
             {
@@ -74,21 +74,76 @@ namespace xls检测更新
             var gameText = workbook.Worksheets["Game-Text"];
             int textColCount = gameText.Columns.Length;
             int textRowCount = gameText.Rows.Length;
-            var textRange = gameText.Range;
-            var textTranslate = new Dictionary<string, Dictionary<string, string>> ();
+            //var textRange = gameText.Range;
+            var textTranslate = new Dictionary<string, Dictionary<string, string>>();
             for (int i = 2; i <= singleRowCount; i++)
             {
                 Dictionary<string, string> singleTextTranslate = new Dictionary<string, string>();
                 for (int j = 1; j <= textColCount; j++)
                 {
                     singleTextTranslate[gameText[1, j].DisplayedText] = gameText[i, j].DisplayedText;
-
                 }
                 textTranslate[gameText[i, 1].DisplayedText] = singleTextTranslate;
             }
             Console.WriteLine(JsonConvert.SerializeObject(textTranslate, Formatting.Indented));
             File.WriteAllText(direPath + @"\Game-Text.json", JsonConvert.SerializeObject(textTranslate, Formatting.Indented));
             Console.WriteLine("///////////////");
+            //加载和储存游戏对话文本系统和各种翻译
+            DialogModel currentDialogModel = new DialogModel();
+            List<DialogModel> dialogModels = new List<DialogModel>();
+            var storyText = workbook.Worksheets["Story"];
+            int storyColCount = storyText.Columns.Length;
+            int storyRowCount = storyText.Rows.Length;
+            for (int i = 2; i <= storyRowCount; i++)
+            {
+                string Tag = storyText[i, 1].DisplayedText;
+                string Branch = storyText[i, 2].DisplayedText;
+                string Chara = storyText[i, 3].DisplayedText;
+                string Position = storyText[i, 4].DisplayedText;
+                string Face = storyText[i, 5].DisplayedText;
+                Dictionary<string, string> text=new Dictionary<string, string>();
+                for (int j = 6; j < storyColCount; j++)
+                {
+                    text[storyText[1, j].DisplayedText] = storyText[i, j].DisplayedText;
+                }
+                if (Chara != "")
+                {
+                    currentDialogModel.Operations.Add(new DialogModel.Operation(Branch, Chara, Position, Face, text));
+                }
+                else
+                {
+                    if (currentDialogModel.Operations.Any())
+                    {
+                        dialogModels.Add(currentDialogModel);
+                        currentDialogModel = new DialogModel();
+                    }
+                }
+            }
+            Console.WriteLine(JsonConvert.SerializeObject(dialogModels, Formatting.Indented));
+            File.WriteAllText(direPath + @"\Story.json", JsonConvert.SerializeObject(dialogModels, Formatting.Indented));
+            Console.WriteLine("///////////////");
+        }
+        class DialogModel
+        {
+            public string Tag { get; set; }
+            public List<Operation> Operations { get; set; } = new List<Operation>();
+            public class Operation
+            {
+
+                public Operation(string branch, string chara, string position, string face, Dictionary<string, string> text)
+                {
+                    Branch = branch;
+                    Chara = chara;
+                    Position = position;
+                    Face = face;
+                    Text = text;
+                }
+                public string Branch { get; set; }//分支
+                public string Chara { get; set; }//角色
+                public string Position { get; set; }//立绘位置
+                public string Face { get; set; }//面部表情
+                public Dictionary<string, string> Text { get; set; }//不同语言的文字
+            }
         }
         class CardModelInfo
         {
@@ -109,14 +164,13 @@ namespace xls检测更新
             public string imageUrl;
             public bool isFinish;
 
-
             public CardModelInfo(Worksheet cards, int i)
             {
 
                 cardID = cards[i, cards.GetIndex("Id")].GetXlsData<int>();
                 level = cards.GetIndex("Level") != 0 ? cards[i, cards.GetIndex("Level")].GetXlsData<string>() : "";
                 series = cards.GetIndex("Series") != 0 ? cards[i, cards.GetIndex("Series")].GetXlsData<string>() : "";
-                tag =  cards[i, cards.GetIndex("Tag")].GetXlsData<string>();
+                tag = cards[i, cards.GetIndex("Tag")].GetXlsData<string>();
                 point = cards[i, cards.GetIndex("Point")].GetXlsData<int>();
                 isFinish = cards[i, cards.GetIndex("Finish")].GetXlsData<int>() == 1;
                 cardType = cards[i, cards.GetIndex("Type")].GetXlsData<string>().ToEnumIndex("单位", "特殊");
