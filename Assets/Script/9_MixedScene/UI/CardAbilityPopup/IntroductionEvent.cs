346cfd37634cd690590ee83c6299565a4ec68f4d
red-gezi <506065448@qq.com>
2022-01-27 00:22:55 +0800

    更新召唤面板

--- Assets/Script/9_MixedScene/UI/CardAbilityPopup/IntroductionEvent.cs
+++ Assets/Script/9_MixedScene/UI/CardAbilityPopup/IntroductionEvent.cs
@@ -2,10 +2,20 @@
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 //对卡组列表卡牌模型中进行相关操作
-public class IntroductionEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class IntroductionEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,IPointerClickHandler
 {
     public UnityEvent onPointerEnter;
     public UnityEvent onPointerExit;
+    public UnityEvent onMouse;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right||eventData.button == PointerEventData.InputButton.Left && eventData.clickTime > 2)
+        {
+            Debug.Log("召唤面板");
+        }
+    }
+
     //public UnityEvent onClick;
     public void OnPointerEnter(PointerEventData eventData) => onPointerEnter.Invoke();
     public void OnPointerExit(PointerEventData eventData) => onPointerExit.Invoke();
