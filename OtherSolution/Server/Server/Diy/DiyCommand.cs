a3b872d384b8a6a0f3c384b62b6a0ec8c4882273
red-gezi <506065448@qq.com>
2022-03-15 18:21:16 +0800

    更新网页端代码

--- OtherSolution/Server/Server/Diy/DiyCommand.cs
+++ OtherSolution/Server/Server/Diy/DiyCommand.cs
@@ -10,94 +10,30 @@ namespace Server
     {
         public static List<DiyCardInfo> GetDiyCardsInfo() => MongoDbCommand.DiyCardCollection.AsQueryable().ToList();
         public static string GetDiyCardsImageUrl(int uid) => GetDiyCardsInfo().FirstOrDefault(x => x.uid == uid)?.imageUrl;
-        //static MongoClient client;
-        //static IMongoDatabase db;
-        //public static IMongoCollection<DiyCardInfo> diyCardCollection;
-        //public static IMongoCollection<DiyCardTextureInfo> diyCardTextureCollection;
-        //public static IMongoCollection<DefaultTexture> defaultTextureCollection;
-        //public static void MongodbConnect()
-        //{
-        //    client = new MongoClient("mongodb://cynthia.ovyno.com:28020");
-        //    db = client.GetDatabase("Gezi");
-        //    diyCardCollection = db.GetCollection<DiyCardInfo>("DiyCards");
-        //    diyCardTextureCollection = db.GetCollection<DiyCardTextureInfo>("DiyCardTexture");
-        //    defaultTextureCollection = db.GetCollection<DefaultTexture>("DefaultTexture");
-        //    Console.WriteLine("数据库初始化完成");
-        //}
-        //public static void InItDefaultTexture(DefaultTexture defaultTexture)
-        //{
-        //    //defaultTextureCollection.InsertOne(defaultTexture);
-        //}
-        //public static void InItDefaultTexture()
-        //{
-        //    //Info.defaultTexture = defaultTextureCollection.AsQueryable().First();
-        //}
-        //public static void InItDiyCardInfos()
-        //{
-
-
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        DiyCardInfo diyCard = new DiyCardInfo()
-        //        {
-        //            cardName = "物体" + i,
-        //            describe = "这是" + i + "的描述",
-        //            commits = new List<DiyCardInfo.Commit>
-        //                {
-        //                    new DiyCardInfo.Commit()
-        //                    {
-        //                        user="gezi",
-        //                        text="好烂"
-        //                    },
-        //                    new DiyCardInfo.Commit()
-        //                    {
-        //                        user="gezi",
-        //                        text="好烂"
-        //                    }
-        //                }
-        //        };
-        //        diyCardCollection.InsertOne(diyCard);
-        //    }
-        //}
-        //public static void AddDiyCardInfos(string name, string describe, byte[] uploadImageData)
-        //{
-        //    int uid = diyCardCollection.AsQueryable().Count();
-        //    DiyCardInfo diyCard = new DiyCardInfo()
-        //    {
-        //        uid= uid,
-        //        cardName = name,
-        //        describe = describe,
-        //        commits = new List<DiyCardInfo.Commit>
-        //                {
-        //                    new DiyCardInfo.Commit()
-        //                    {
-        //                        user="gezi",
-        //                        text="好烂"
-        //                    },
-        //                    new DiyCardInfo.Commit()
-        //                    {
-        //                        user="gezi",
-        //                        text="好烂"
-        //                    }
-        //                }
-        //    };
-        //    DiyCardTextureInfo diyCardTextureInfo = new DiyCardTextureInfo()
-        //    {
-        //        uid = uid,
-        //        imagedata = uploadImageData,
-        //    };
-        //    diyCardCollection.InsertOne(diyCard);
-        //    diyCardTextureCollection.InsertOne(diyCardTextureInfo);
-        //}
-        //public static void GetDiyCardsInfo()
-        //{
-        //    Info.DiyCardInfo = diyCardCollection.AsQueryable().ToList();
-        //}
-        //public static byte[] GetDiyTexture(int uid)
-        //{
-        //   return diyCardTextureCollection.Find(info => info.uid == uid).First().imagedata;
-        //}
+        public static void AddDiyCardInfos(string name, string describe,string imageUrl)
+        {
+            int uid = GetDiyCardsInfo().Count();
+            DiyCardInfo diyCard = new DiyCardInfo()
+            {
+                uid = uid,
+                cardName = name,
+                describe = describe,
+                imageUrl = imageUrl,
+                commits = new List<DiyCardInfo.Commit>
+                        {
+                            new DiyCardInfo.Commit()
+                            {
+                                user="gezi",
+                                text="好烂"
+                            },
+                            new DiyCardInfo.Commit()
+                            {
+                                user="gezi",
+                                text="好烂"
+                            }
+                        }
+            };
+            MongoDbCommand.DiyCardCollection.InsertOne(diyCard);
+        }
     }
 }
-
-
