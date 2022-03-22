69d448e22964bf8a35709f85047da9196a2f249b
red-gezi <506065448@qq.com>
2022-03-23 00:39:33 +0800

    新增卡牌

---
+++ Assets/Script/9_MixedScene/CardSpace/Card2103004.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Collections.Generic;
+using TouhouMachineLearningSummary.Model;
+using TouhouMachineLearningSummary.GameEnum;
+namespace TouhouMachineLearningSummary.CardSpace
+{
+    /// <summary>
+    /// 卡牌名称:充能水炮
+    /// </summary>
+    public class Card2103004 : Card
+    {
+        public override void Init()
+        {
+            //初始化通用卡牌效果
+            base.Init();
+            AbalityRegister(TriggerTime.When, TriggerType.Play)
+               .AbilityAdd(async (triggerInfo) =>
+               {
+                   await GameSystem.SelectSystem.SelectLocation(this, CardDeployTerritory, CardDeployRegion);
+                   await GameSystem.TransSystem.DeployCard(new TriggerInfoModel(this,this));
+               })
+               .AbilityAppend();
+        }
+    }
+}
---
+++ Assets/Script/9_MixedScene/CardSpace/Card2103005.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Collections.Generic;
+using TouhouMachineLearningSummary.Model;
+using TouhouMachineLearningSummary.GameEnum;
+namespace TouhouMachineLearningSummary.CardSpace
+{
+    /// <summary>
+    /// 卡牌名称:河童修理工
+    /// </summary>
+    public class Card2103005 : Card
+    {
+        public override void Init()
+        {
+            //初始化通用卡牌效果
+            base.Init();
+            AbalityRegister(TriggerTime.When, TriggerType.Play)
+               .AbilityAdd(async (triggerInfo) =>
+               {
+                   await GameSystem.SelectSystem.SelectLocation(this, CardDeployTerritory, CardDeployRegion);
+                   await GameSystem.TransSystem.DeployCard(new TriggerInfoModel(this,this));
+               })
+               .AbilityAppend();
+        }
+    }
+}
