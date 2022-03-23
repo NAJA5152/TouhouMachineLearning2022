149e1debc7ed74a31582c5d77a6e766981eb1ebb
red-gezi <506065448@qq.com>
2022-03-23 17:58:12 +0800

    完善部分卡牌效果

--- Assets/Script/9_MixedScene/CardSpace/Card2102002.cs
+++ Assets/Script/9_MixedScene/CardSpace/Card2102002.cs
@@ -5,7 +5,7 @@ using TouhouMachineLearningSummary.GameEnum;
 namespace TouhouMachineLearningSummary.CardSpace
 {
     /// <summary>
-    /// 卡牌名称:充能箱
+    /// 卡牌名称:应急充能箱
     /// </summary>
     public class Card2102002 : Card
     {
@@ -18,6 +18,21 @@ namespace TouhouMachineLearningSummary.CardSpace
                {
                    await GameSystem.SelectSystem.SelectLocation(this, CardDeployTerritory, CardDeployRegion);
                    await GameSystem.TransSystem.DeployCard(new TriggerInfoModel(this,this));
+               })
+               .AbilityAppend();
+
+            AbalityRegister(TriggerTime.When, TriggerType.Deploy)
+               .AbilityAdd(async (triggerInfo) =>
+               {
+                   await GameSystem.SelectSystem.SelectUnite(this, GameSystem.InfoSystem.AgainstCardSet[CardTag.Machine][Orientation.My][GameRegion.Grave].CardList, 1);
+
+                   var cardList = GameSystem.InfoSystem.AgainstCardSet[Orientation.My][GameRegion.Battle][CardField.Energy].CardList;
+                   int num = cardList.Sum(card => card[CardField.Energy]);
+                   await GameSystem.FieldSystem.SetField(new TriggerInfoModel(this, cardList).SetTargetField(CardField.Energy, 0));
+                   for (int i = 0; i < num; i++)
+                   {
+                       await GameSystem.PointSystem.Hurt(new TriggerInfoModel(this, GameSystem.InfoSystem.AgainstCardSet[Orientation.Op][GameRegion.Battle][CardRank.Silver][CardRank.Copper][CardFeature.Largest].CardList.FirstOrDefault()));
+                   }
                })
                .AbilityAppend();
         }
