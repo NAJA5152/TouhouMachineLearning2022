521518e4284f8db9ec6efef7a158fe4a79f7bdef
red-gezi <506065448@qq.com>
2022-04-13 16:55:45 +0800

    优化写法

--- OtherSolution/ServerVersions/VersionsHub.cs
+++ OtherSolution/ServerVersions/VersionsHub.cs
@@ -11,28 +11,23 @@ public class VersionsHub : Hub
         progress = Process.Start("dotnet", "Server/Server.dll");
     }
     public void StartServer() => progress = Process.Start("dotnet", "Server/Server.dll");
-    public void CloseServer()
-    {
-        progress.Kill();
-    }
+    public void CloseServer() => progress.Kill();
 
     public bool UpdateServer(byte[] datas)
     {
-        Console.WriteLine("收到更新信息");
-        CloseServer();
-        File.WriteAllBytes("Server/Server.dll", datas);
-        Console.WriteLine("进行版本更新,当前服务器更新时间为" + new FileInfo("Server/Server.dll").LastWriteTime);
-        StartServer();
-        return true;
+        try
+        {
+            Console.WriteLine("收到更新信息");
+            CloseServer();
+            File.WriteAllBytes("Server/Server.dll", datas);
+            Console.WriteLine("进行版本更新,当前服务器更新时间为" + new FileInfo("Server/Server.dll").LastWriteTime);
+            StartServer();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
     }
 }
-
-//try
-//{
-
-//}
-//catch (Exception e)
-//{
-//    Console.WriteLine(e.Message);
-//    return false;
-//}
