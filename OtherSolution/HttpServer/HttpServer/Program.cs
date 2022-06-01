2ae0ea723dfa4dcb7d9fe67dd83e305c5acc9170
red-gezi <506065448@qq.com>
2022-06-01 08:30:40 +0800

    更新httpserver

--- OtherSolution/HttpServer/HttpServer/Program.cs
+++ OtherSolution/HttpServer/HttpServer/Program.cs
@@ -1,159 +1,112 @@
-﻿using System;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 class MyWebServer
 {
-    public static void Main()
-    {
-        MyWebServer MWS = new MyWebServer();
-    }
-    private TcpListener myListener;
-    public MyWebServer()
-    {
-        //开始兼听端口
-        myListener = new TcpListener(8080);
-        myListener.Start();
-        Console.WriteLine("启动服务器");
-        Thread th = new Thread(new ThreadStart(StartListen));
-        th.Start();
 
-        public void StartListen()
+
+
+    public static  void Main()
+    {
+        Task.Run(async () => {
+            int num = 0;
+            Console.WriteLine(DateTime.Now);
+            string text = await add1(ref num);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(text);
+            Console.WriteLine(num);
+        });
+        
+        Task<string> add1(ref int num)
         {
-            int iStartPos = 0;
-            String sRequest;
-            String sDirName;
-            String sRequestedFile;
-            String sErrorMessage;
-            String sLocalDir;
-            String sMyWebServerRoot = Directory.GetCurrentDirectory(); //设置你的虚拟目录
-            String sPhysicalFilePath = "";
-            String sFormattedMessage = "";
-            String sResponse = "";
-            while (true)
+            num++;
+            return delay(num);
+            async Task<string> delay(int num)
             {
-                //接受新连接
-                Socket mySocket = myListener.AcceptSocket();
-                Console.WriteLine("Socket Type " + mySocket.SocketType);
-                if (mySocket.Connected)
+                await Task.Delay(1000);
+                return "succerss";
+            }
+        }
+
+        TcpListener server = new TcpListener(IPAddress.Parse("0.0.0.0"), 7777);
+        server.Start();
+        Console.WriteLine("启动服务器");
+        while (true)
+        {
+            //接受新连接
+            Socket socket = server.AcceptSocket();
+            Task.Run(() =>
+            {
+                Socket currentClient = socket;
+                if (currentClient.Connected)
                 {
-                    Console.WriteLine("\nClient Connected!!\n==================\nCLient IP {0}\n", mySocket.RemoteEndPoint);
-                    Byte[] bReceive = new Byte[1024];
-                    int i = mySocket.Receive(bReceive, bReceive.Length, 0);
+                    Console.WriteLine($"{currentClient.RemoteEndPoint}已连接");
+                    Byte[] receive = new Byte[1024];
+                    currentClient.Receive(receive, receive.Length, 0);
                     //转换成字符串类型
-                    string sBuffer = Encoding.ASCII.GetString(bReceive);
+                    string buffer = Encoding.ASCII.GetString(receive);
                     //只处理"get"请求类型
-                    if (sBuffer.Substring(0, 3) != "GET")
+                    if (buffer.Substring(0, 3) != "GET")
                     {
                         Console.WriteLine("只处理get请求类型..");
-                        mySocket.Close();
+                        currentClient.Close();
                         return;
                     }
                     // 查找 "HTTP" 的位置
-                    iStartPos = sBuffer.IndexOf("HTTP", 1);
-                    string sHttpVersion = sBuffer.Substring(iStartPos, 8);
+                    int startPoint = buffer.IndexOf("HTTP", 1);
+                    string httpVersion = buffer.Substring(startPoint, 8);
                     // 得到请求类型和文件目录文件名
-                    sRequest = sBuffer.Substring(0, iStartPos - 1);
-                    sRequest.Replace("\\", "/");
-                    //如果结尾不是文件名也不是以"/"结尾则加"/"
-                    if ((sRequest.IndexOf(".") < 1) && (!sRequest.EndsWith("/")))
-                    {
-                        sRequest = sRequest + "/";
-                    }
+                    string request = buffer.Substring(0, startPoint - 1);
+                    request.Replace("\\", "/");
+
                     //得带请求文件名
-                    iStartPos = sRequest.LastIndexOf("/") + 1;
-                    sRequestedFile = sRequest.Substring(iStartPos);
-                    //得到请求文件目录
-                    sDirName = sRequest.Substring(sRequest.IndexOf("/"), sRequest.LastIndexOf("/") - 3);
+                    startPoint = request.IndexOf("/") + 1;
+                    string filePath = request.Substring(startPoint);
                     //获取虚拟目录物理路径
-                    sLocalDir = sMyWebServerRoot;
-                    Console.WriteLine("请求文件目录 : " + sLocalDir);
-                    if (sLocalDir.Length == 0)
+                    var targetFile = new FileInfo(Directory.GetCurrentDirectory() + "/" + (filePath == "" ? "index.html" : filePath));
+                    Console.WriteLine("请求文件: " + targetFile.FullName);
+                    if (targetFile.Exists)
                     {
-                        sErrorMessage = "Error!! Requested Directory does not exists";
-                        SendHeader(sHttpVersion, "", sErrorMessage.Length, " 404 Not Found", ref mySocket);
-                        SendToBrowser(sErrorMessage, ref mySocket);
-                        mySocket.Close();
-                        continue;
-                    }
-                    if (sRequestedFile.Length == 0)
-                    {
-                        // 取得请求文件名
-                        sRequestedFile = "index.HTML";
-                    }
-                    // 取得请求文件类型(设定为text/HTML)
-                    String sMimeType = "text/HTML";
-                    sPhysicalFilePath = sLocalDir + sRequestedFile;
-                    Console.WriteLine("请求文件: " + sPhysicalFilePath);
-                    if (File.Exists(sPhysicalFilePath) == false)
-                    {
-                        sErrorMessage = "404 Error! File Does Not Exists...";
-                        SendHeader(sHttpVersion, "", sErrorMessage.Length, " 404 Not Found", ref mySocket);
-                        SendToBrowser(sErrorMessage, ref mySocket);
-                        Console.WriteLine(sFormattedMessage);
-                    }
-                    else
-                    {
-                        int iTotBytes = 0;
-                        sResponse = "";
-                        FileStream fs = new FileStream(sPhysicalFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                        int dataPoint = 0;
+                        string sResponse = "";
+                        FileStream fs = new FileStream(targetFile.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
                         BinaryReader reader = new BinaryReader(fs);
                         byte[] bytes = new byte[fs.Length];
                         int read;
                         while ((read = reader.Read(bytes, 0, bytes.Length)) != 0)
                         {
                             sResponse = sResponse + Encoding.ASCII.GetString(bytes, 0, read);
-                            iTotBytes = iTotBytes + read;
+                            dataPoint = dataPoint + read;
                         }
                         reader.Close();
                         fs.Close();
-                        SendHeader(sHttpVersion, sMimeType, iTotBytes, " 200 OK", ref mySocket);
-                        SendToBrowser(bytes, ref mySocket);
+
+                        string contentType = targetFile.Extension == ".html" ? "text/HTML" : "application/octet-stream";
+                        SendHeader(httpVersion, contentType, dataPoint, " 200 OK", currentClient);
+                        SendToBrowser(bytes, currentClient);
                     }
-                    mySocket.Close();
+                    else
+                    {
+                        string message = "404";
+                        SendHeader(httpVersion, "text/HTML", message.Length, " 404 Not Found", currentClient);
+                        SendToBrowser(Encoding.ASCII.GetBytes(message), currentClient);
+                    }
+                    currentClient.Close();
                 }
-            }
+            });
         }
     }
-    public void SendHeader(string sHttpVersion, string sMIMEHeader, int iTotBytes, string sStatusCode, ref Socket mySocket)
+    static void SendHeader(string sHttpVersion, string contentType, int iTotBytes, string sStatusCode, Socket mySocket)
     {
-        String sBuffer = "";
-        if (sMIMEHeader.Length == 0)
-        {
-            sMIMEHeader = "text/HTML"; // 默认 text/HTML
-        }
+        string sBuffer = "";
         sBuffer = sBuffer + sHttpVersion + sStatusCode + "\r\n";
         sBuffer = sBuffer + "Server: cx1193719-b\r\n";
-        sBuffer = sBuffer + "Content-Type: " + sMIMEHeader + "\r\n";
+        sBuffer = sBuffer + $"Content-Type: {contentType}\r\n";
         sBuffer = sBuffer + "Accept-Ranges: bytes\r\n";
         sBuffer = sBuffer + "Content-Length: " + iTotBytes + "\r\n\r\n";
         Byte[] bSendData = Encoding.ASCII.GetBytes(sBuffer);
-        SendToBrowser(bSendData, ref mySocket);
+        SendToBrowser(bSendData, mySocket);
         Console.WriteLine("Total Bytes : " + iTotBytes.ToString());
     }
-    public void SendToBrowser(String sData, ref Socket mySocket) => SendToBrowser(Encoding.ASCII.GetBytes(sData), ref mySocket);
-    public void SendToBrowser(Byte[] bSendData, ref Socket mySocket)
-    {
-        int numBytes = 0;
-        try
-        {
-            if (mySocket.Connected)
-            {
-                if ((numBytes = mySocket.Send(bSendData, bSendData.Length, 0)) == -1)
-                    Console.WriteLine("Socket Error cannot Send Packet");
-                else
-                {
-                    Console.WriteLine("No. of bytes send {0}", numBytes);
-                }
-            }
-            else
-                Console.WriteLine("连接失败....");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("发生错误 : {0} ", e);
-        }
-    }
+    static void SendToBrowser(Byte[] bSendData, Socket mySocket) => mySocket.Send(bSendData, bSendData.Length, 0);
 }
