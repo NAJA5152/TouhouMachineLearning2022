d130aba20c15f5b3cae27d9c1611b6be04faf4ca
red-gezi <506065448@qq.com>
2022-06-09 00:54:31 +0800

    Update CardLibraryCommand.cs

--- Assets/Script/1_LoginSceen/PageComponents/牌库组件/CardLibraryCommand.cs
+++ Assets/Script/1_LoginSceen/PageComponents/牌库组件/CardLibraryCommand.cs
@@ -15,21 +15,21 @@ namespace TouhouMachineLearningSummary.Command
         {
             //牌库卡牌列表
             //var showCardList = CardAssemblyManager.GetLastMultiCardInfos;
-            
+
             Info.PageCompnentInfo.LibraryFilterCardList = CardAssemblyManager.GetLastMultiCardInfos;
             //如果当前是编辑卡组模式，则只显示指定阵营
             if (Info.PageCompnentInfo.isEditDeckMode)
             {
                 //showCardList = showCardList.Where(card => card.cardCamp == Info.CardCompnentInfo.targetCamp).ToList();
                 //showCardList = showCardList.Where(card => card.cardRank == GameEnum.CardRank.Silver).ToList();
-                Info.PageCompnentInfo.LibraryFilterCardList = Info.PageCompnentInfo.LibraryFilterCardList.Where(card => card.cardCamp == Info.PageCompnentInfo.targetCamp|| card.cardCamp ==  GameEnum.Camp.Neutral).ToList();
+                Info.PageCompnentInfo.LibraryFilterCardList = Info.PageCompnentInfo.LibraryFilterCardList.Where(card => card.cardCamp == Info.PageCompnentInfo.targetCamp || card.cardCamp == GameEnum.Camp.Neutral).ToList();
                 //Info.CardCompnentInfo.LibraryFilterCardList = Info.CardCompnentInfo.LibraryFilterCardList.Where(card => card.cardRank == GameEnum.CardRank.Silver).ToList();
             }
             int libraryCardNumber = Info.PageCompnentInfo.LibraryFilterCardList.Count();
             //如果处于卡组编辑状态，则对卡牌列表做个筛选
             //已生成卡牌列表
             //清空所有已销毁卡牌
-            Info.PageCompnentInfo.libraryCardModels.RemoveAll(null);
+            Info.PageCompnentInfo.libraryCardModels = Info.PageCompnentInfo.libraryCardModels.Where(model => model != null).ToList();
             int libraryModelNumber = Info.PageCompnentInfo.libraryCardModels.Count;
             var s = Info.PageCompnentInfo.libraryCardModels;
             Info.PageCompnentInfo.libraryCardModels.ForEach(model => model.SetActive(false));
@@ -59,7 +59,7 @@ namespace TouhouMachineLearningSummary.Command
                 newCardModel.GetComponent<Image>().sprite = cardTex;
                 newCardModel.transform.GetChild(1).GetComponent<Text>().text = info.TranslateName;
                 newCardModel.transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<Text>().text = "X" + (cardNum > 9 ? "9+" : cardNum + "");
-                newCardModel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = info.point==0?"": info.point.ToString();
+                newCardModel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = info.point == 0 ? "" : info.point.ToString();
                 newCardModel.GetComponent<Image>().color = new Color(1, 1, 1, cardNum == 0 ? 0.2f : 1);
                 newCardModel.SetActive(true);
             }
