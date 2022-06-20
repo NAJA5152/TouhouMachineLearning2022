c58b37d2657306a08397495fbcb2c9e85019ee16
red-gezi <506065448@qq.com>
2022-06-21 00:34:23 +0800

    更新热更机制

--- Assets/Script/0_Other/HotFixedManager.cs
+++ Assets/Script/0_Other/HotFixedManager.cs
@@ -27,7 +27,7 @@ public class HotFixedManager : MonoBehaviour
     async void Start()
     {
         RestartNotice.transform.localScale = new Vector3(1, 0, 1);
-        versiousText.text = "v6";
+        versiousText.text = "我要改成v7";
         ConfigManager.InitConfig();
         loadText.text = "初始化网络";
         _ = NetCommand.Init();
