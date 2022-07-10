9de4cb35a44334a6d2bd4bd08ad43fe44c3b9d70
red-gezi <506065448@qq.com>
2022-07-11 00:08:58 +0800

    更新桌面

---
+++ Assets/Script/9_MixedScene/Shader/Blur.cs
@@ -0,0 +1,24 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blur : MonoBehaviour
+{
+    public Material material;
+    public RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScreenCapture.CaptureScreenshotIntoRenderTexture(renderTexture);
+        GetComponent<Image>().material.SetTexture("_MainTex", renderTexture);
+    }
+    [Button("НиЭМ")]
+    public void Test()
+    {
+        ScreenCapture.CaptureScreenshotIntoRenderTexture(renderTexture);
+        GetComponent<Image>().material.SetTexture("_MainTex", renderTexture);
+    }
+}
