ac5280fd4346522b1fc1a7adda2bfaae6dae2f16
red-gezi <506065448@qq.com>
2022-08-22 18:01:16 +0800

    更新live2d脚本相关控制代码

--- Assets/Script/9_MixedScene/Dialogue/Live2d/Live2dTest.cs
+++ Assets/Script/9_MixedScene/Dialogue/Live2d/Live2dTest.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Live2D.Cubism.Core;
+using System.Linq;
+using Live2D.Cubism.Rendering;
+using Sirenix.OdinInspector;
+using UnityEditor.Animations;
+using UnityEditor;
 
 public class Live2dTest : MonoBehaviour
 {
     public CubismModel _model;
+    //public AnimatorController animator;
+    public Animator animator;
+    public List<AnimationClip> animationClips;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +21,35 @@ public class Live2dTest : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void LateUpdate() => _model.ForceUpdateNow();
+    [Button("变灰")]
+    void Togray() => _model.Drawables.ToList().ForEach(drawable => drawable.GetComponent<CubismRenderer>().Color = Color.gray);
+    [Button("变白")]
+    void ToWhite() => _model.Drawables.ToList().ForEach(drawable => drawable.GetComponent<CubismRenderer>().Color = Color.white);
+    [Button("初始化动画片段")]
+    void Init()
     {
-        _model.ForceUpdateNow();
+        animator = GetComponent<Animator>();
+        AnimatorController animatorController = animator.runtimeAnimatorController as AnimatorController;
+        animatorController.parameters = new AnimatorControllerParameter[0];
+        animationClips.ForEach(clip => animatorController.AddParameter(clip.name, AnimatorControllerParameterType.Trigger));
+        AnimatorStateMachine stateMachine = animatorController.layers[0].stateMachine;
+        stateMachine.states = new ChildAnimatorState[0];
+        AnimatorState state = stateMachine.AddState("idle");
+        state.motion = animationClips[0];
+        stateMachine.AddEntryTransition(state);
+        animationClips.ForEach(clip =>
+        {
+            AnimatorState state = stateMachine.AddState(clip.name);
+            state.motion = clip;
+            AnimatorStateTransition transition = stateMachine.AddAnyStateTransition(state);
+            transition.AddCondition(AnimatorConditionMode.If, 1, clip.name);
+        });
+        AssetDatabase.SaveAssets();
+    }
+    [Button("播放")]
+    void Play(string tag)
+    {
+        GetComponent<Animator>().SetTrigger(tag);
     }
 }
